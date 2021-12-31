9f607dda95e50f5ad779d6d6833f72e1a40acdfb
captainfiz <rijban.khan@adglobal360.com>
2021-12-31 22:26:25 +0530

    api layer

--- routes/crypto.js
+++ routes/crypto.js
@@ -4,39 +4,40 @@ const axios = require('axios')
 
 /* GET home page. */
 router.get('/live', function (req, res, next) {
-  const {currencies, source } = req.query
+  const { currencies, source } = req.query
   axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${source}&ids=${currencies}`)
-  .then(function (response) {
-    const filterCrypto = response.data.map(val=>{
-      return {
-        [val.symbol.toLocaleUpperCase()]:{
-          id:val.id,
-          symbol:val.symbol.toLocaleUpperCase(),
-          name:val.name,
-          slug:val.name,
-          quote: {
-            [val.symbol.toLocaleUpperCase()]: {
+    .then(function (response) {
+      const filterCrypto = response.data.map(val => {
+        return {
+          [val.symbol.toLocaleUpperCase()]: {
+            id: val.id,
+            symbol: val.symbol.toLocaleUpperCase(),
+            name: val.name,
+            slug: val.name,
+            quote: {
+              [val.symbol.toLocaleUpperCase()]: {
                 price: val.current_price,
+              }
             }
+          }
         }
-        }
-      }
+      })
+      const liveCrypto = Object.assign({}, ...filterCrypto);
+      res.json({
+        data: liveCrypto
+      });
     })
-    res.json({
-      data:{...filterCrypto}
-    });
-  })
-  .catch(function (error) {
-    res.json({
-      "status": {
+    .catch(function (error) {
+      res.json({
+        "status": {
           "timestamp": "2021-12-31T14:28:55.193Z",
           "error_code": 1002,
           "error_message": "API key missing.",
           "elapsed": 0,
           "credit_count": 0
-      }
-  });
-  })
+        }
+      });
+    })
 
 });
 
