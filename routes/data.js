f5fc43d9b897778d483e0f5eb8d68c386e5f17c0
captainfiz <rijban.khan@adglobal360.com>
2022-01-10 17:13:29 +0530

    make it better

--- routes/data.js
+++ routes/data.js
@@ -1,25 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios')
-
+// https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,INR
 /* GET home page. */
 router.get('/top/mktcapfull', function (req, res, next) {
-    const { limit, tsym, api_key } = req.query
-    axios.get(`https://min-api.cryptocompare.com/data/top/mktcapfull?limit=${limit}&tsym=${tsym}`)
-        .then((response)=>{
-            res.json(response);
-        })
-        .catch((error)=>{
-            res.json({
-                "Response": "Error",
-                "Message": "tsym param is not valid. Nothing trades in USA",
-                "HasWarning": false,
-                "Type": 2,
-                "RateLimit": {},
-                "Data": {},
-                "ParamWithError": "tsym"
-            });
-        })
+  const { limit, tsym,api_key } = req.query
+  axios.get(`https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD`)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      res.json(error);
+    })
 
 });
 
