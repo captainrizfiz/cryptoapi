13436b8c995fb0252b0ad697ec7866fa6f61563c
captainfiz <rijban.khan@adglobal360.com>
2022-01-11 17:03:22 +0530

    make it better

--- routes/data.js
+++ routes/data.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 // https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,INR
 /* GET home page. */
 router.get('/top/mktcapfull', function (req, res, next) {
-  const { limit, tsym,api_key } = req.query
+  const { limit, tsym, api_key } = req.query
   axios.get(`https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD`)
     .then(function (response) {
       res.json(response.data);
@@ -17,15 +17,26 @@ router.get('/top/mktcapfull', function (req, res, next) {
 
 
 router.get('/price', function (req, res, next) {
-    const { fsym, tsyms,api_key } = req.query
-    res.json({"Response":"Error","Message":"cccagg_or_exchange market does not exist for this coin pair (%S-USD)","HasWarning":false,"Type":1,"RateLimit":{},"Data":{},"Cooldown":0});
-  
+  const { fsym, tsyms, api_key } = req.query
+  axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${fsym}&tsyms=${tsyms}`)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      res.json(error);
+    })
 });
-  
+
 
 router.get('/v2/histominute', function (req, res, next) {
-    const { fsym, tsyms,api_key,limit } = req.query
-    res.json({"Response":"Error","Message":"CCCAGG market does not exist for this coin pair (%S-USD)","HasWarning":false,"Type":1,"RateLimit":{},"Data":{},"Cooldown":0});
-  
+  const { fsym, tsyms, api_key, limit } = req.query
+  axios.get(`https://min-api.cryptocompare.com/data/v2/histominute?fsym=${fsym}&tsym=${tsyms}&limit=${limit}`)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      res.json(error);
+    })
+
 });
 module.exports = router;
